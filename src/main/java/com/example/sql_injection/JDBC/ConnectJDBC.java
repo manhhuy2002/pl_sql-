a92b1c1fc15ba34ebe0d52a6121038f5e875d85f
manhhuy2002 <huymanhtran08112002@gmail.com>
2023-06-01 14:28:35 +0700

    first push

--- src/main/java/com/example/sql_injection/JDBC/ConnectJDBC.java
+++ src/main/java/com/example/sql_injection/JDBC/ConnectJDBC.java
@@ -4,20 +4,22 @@ import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Component
 public class ConnectJDBC {
     public Connection getConnection(){
         Connection connection = null;
         try{
-            Class.forName("org.postgresql.Driver"); // Thêm dòng này để tải lớp Driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Thêm dòng này để tải lớp Driver
 //            String url = "jdbc:postgresql://127.0.0.1:5432/postgres";
-            String url = "jdbc:postgresql://192.168.220.128:5432/postgres";
-            String username = "postgres";
+//            String url = "jdbc:postgresql://192.168.220.128:5432/postgres";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=mssql";
+            String username = "sa";
             String password = "manhhuy2002";
             connection = DriverManager.getConnection(url, username, password);
 
-        } catch (Exception exception){
+        } catch (ClassNotFoundException | SQLException exception){
             exception.printStackTrace();
         }
         return connection;
